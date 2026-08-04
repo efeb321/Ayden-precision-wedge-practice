1672e6f7b60d9315f5b715581c474d12504ba2f7
efeb321 <e.febressr@gmail.com>
2026-08-04 11:25:57 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='precision-beta-v11';
+const CACHE='precision-beta-v12';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
 self.addEventListener('install',event=>{
   event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
